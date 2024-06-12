dbb7834398560397d108cd6a76561ab6ffad69a7
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-12 09:25:43 +0300

    0. Async Generator (automated command)

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float]:
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
