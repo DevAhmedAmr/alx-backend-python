9d3679afa95fc7bb36fdba07fd3077e6124b1836
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-12 09:53:26 +0300

    0. Async Generator (automated command)

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,12 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
-    return list(async_generator())
+    """
+        collect 10 random numbers using an async comprehension
+        over async_generator, then return the 10 random numbers.
+
+    """
+    return [num async for num in async_generator()]
 
 
 async def main():
