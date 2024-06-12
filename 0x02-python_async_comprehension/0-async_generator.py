11569df8a69bf77a7b30402cfa6642efc3328f40
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-12 09:29:26 +0300

    0. Async Generator (automated command)

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,24 +5,24 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-	"""he coroutine will loop 10 times,
-	each time asynchronously wait 1 second,
-    then yield a random number between 0 and 10
+    """
+        he coroutine will loop 10 times, each time asynchronously wait 1 second,
+        then yield a random number between 0 and 10
 
-	Yields:
-		Generator[float, None, None]: _description_"""
+            Yields:
+                    Generator[float, None, None]: _description_ """
+
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
 
 
-# if "__main__" == __name__:
-#     import asyncio
+if "__main__" == __name__:
 
-#     async def print_yielded_values():
-#         result = []
-#         async for i in async_generator():
-#             result.append(i)
-#         print(result)
+    async def print_yielded_values():
+        result = []
+        async for i in async_generator():
+            result.append(i)
+        print(result)
 
-# asyncio.run(print_yielded_values())
+asyncio.run(print_yielded_values())
