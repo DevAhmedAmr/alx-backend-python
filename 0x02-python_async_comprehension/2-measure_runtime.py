9b7826836c3e0ae823b764461d5ca643db26e806
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-06-13 11:05:58 +0300

    2. Run time for four parallel comprehensions (automated command)

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -17,10 +17,7 @@ async def measure_runtime() -> float:
     """
     start_time = time.perf_counter()
 
-    list1, list2, list3, lst4 = await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension())
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
 
     return time.perf_counter() - start_time
