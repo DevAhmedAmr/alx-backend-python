5ef096cc92e1c2a181549423668849db81c8b092
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-29 12:26:01 +0300

    added documentation string for TestAccessNestedMap.test_access_nested_map_exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,11 +21,17 @@ class TestAccessNestedMap(TestCase):
 
         Args:
             dictionary (dict): dictionary to be tested
-            path ([type]): path of the info
-            result ([type]): expected result
+            path ([list]): path of the info
+            result ([any]): expected result
         """
         self.assertAlmostEqual(result, access_nested_map(dictionary, path,))
 
     @parameterized.expand([({}, ("a",))])
     def test_access_nested_map_exception(self, dictionary, path):
+        """Raises an exception if the key is not a nested_nested_map attribute .
+
+        Args:
+            dictionary (dict): dictionary to be tested
+            path ([list]): path of the info
+        """
         self.assertRaises(KeyError, access_nested_map, dictionary, path,)
