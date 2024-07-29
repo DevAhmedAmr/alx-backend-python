289089f50e509cd10a06b4370dbc4cda5239d568
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-29 23:50:05 +0300

    testing some stuff (automated command)

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,7 +11,7 @@ from client import (
     GithubOrgClient
 )
 from fixtures import TEST_PAYLOAD
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 import utils
 
 
@@ -41,24 +41,65 @@ class TestGithubOrgClient(unittest.TestCase):
             )
             public_repos_url.assert_called()
 
-    @patch.object(utils, "get_json")
+    @patch('client.get_json')
     def test_public_repos(self, get_json):
-        get_json.return_value = {"mocked_payload", "mocked_value"}
-        with patch.object(GithubOrgClient, "public_repos") as public_repos_url:
-            public_repos_url.return_value = {
-                'truth', 'ruby-openid-apps-discovery', 'autoparse',
-                'anvil-build', 'googletv-android-samples', 'ChannelPlate',
-                'GL-Shader-Validator', 'qpp', 'CSP-Validator', 'embed-dart-vm',
-                'module-server', 'cxx-std-draft', 'filesystem-proposal', 'libcxx',
-                'tracing-framework', 'namebench', 'devtoolsExtended', 'sirius',
-                'testRunner', 'crx2app', 'episodes.dart', 'cpp-netlib', 'dagger',
-                'ios-webkit-debug-proxy', 'google.github.io', 'kratu', 'build-debian-cloud',
-                'traceur-compiler', 'firmata.py', 'vector_math.dart',
-            }
+        get_json.return_value = [{"name": "Google"}, {"name": "Twitter"}]
 
-            client = GithubOrgClient("google")
+        with patch.object(GithubOrgClient, "_public_repos_url",
+                          new_callable=PropertyMock) as public_repos_url:
+            public_repos_url.return_value = " https://hello/world.com"
+            client = GithubOrgClient('test')
+            result = client.public_repos()
+
             self.assertIsNotNone(client.public_repos())
-            self.assertIsInstance(client.public_repos(), set)
-            utils.get_json("test")
+            self.assertIsInstance(client.public_repos(), list)
             public_repos_url.assert_called()
             get_json.assert_called_once()
+######################################
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ Class for Integration test of fixtures """
+
+    @classmethod
+    def setUpClass(cls):
+        """A class method called before tests in an individual class are run"""
+
+        config = {'return_value.json.side_effect':
+                  [
+                      cls.org_payload, cls.repos_payload,
+                      cls.org_payload, cls.repos_payload
+                  ]
+                  }
+        cls.get_patcher = patch('requests.get', **config)
+
+        cls.mock = cls.get_patcher.start()
+
+    def test_public_repos(self):
+        """ Integration test: public repos"""
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.org, self.org_payload)
+        self.assertEqual(test_class.repos_payload, self.repos_payload)
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.mock.assert_called()
+
+    def test_public_repos_with_license(self):
+        """ Integration test for public repos with License """
+        test_class = GithubOrgClient("google")
+
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("XLICENSE"), [])
+        self.assertEqual(test_class.public_repos(
+            "apache-2.0"), self.apache2_repos)
+        self.mock.assert_called()
+
+    @classmethod
+    def tearDownClass(cls):
+        """A class method called after tests in an individual class have run"""
+        cls.get_patcher.stop()
