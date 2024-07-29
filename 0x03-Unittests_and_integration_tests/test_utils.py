06a2535360d8f2819fde81ea454a08aeeee8234a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-29 17:45:56 +0300

    testing some stuff (automated command)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -60,29 +60,28 @@ class TestGetJson(TestCase):
         # Define the TestClass inside the test method
 
 
-class TestClass:
-    """wrapper class for memoize method
-    """
-
-    def a_method(self):
-        return 42
-
-    @memoize
-    def a_property(self):
-        return self.a_method()
-
-
 class TestMemoize(TestCase):
-    """
-    Test memoization of property access.
+    """Memoize test class
     """
 
     def test_memoize(self):
+        """Test memoize method to ensure that when a_property method
+         called twice is correctly tested by calling a_method once
+        """
 
-        # Create an instance of TestClass
-        with mock.patch.object(TestClass, "a_method") as a_method:
+        class TestClass:
+            """wrapper class for memoize method
+            """
 
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with mock.patch.object(TestClass, 'a_method') as _mock:
             test_class = TestClass()
             test_class.a_property()
             test_class.a_property()
-            a_method.assert_called_once()
+            _mock.assert_called_once()
