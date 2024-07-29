f572301c27eed2ad467a7385b41ee92affd036bb
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-07-29 17:50:21 +0300

    testing some stuff (automated command)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -61,13 +61,18 @@ class TestGetJson(TestCase):
 
 
 class TestMemoize(TestCase):
-    ''' memoize unittest '''
+    """
+    Defines test for memoize utility function
+    """
 
-    def test_memoize(self):
-        ''' memoize test '''
-
+    def test_memoize(self) -> None:
+        """
+        Test memoize function
+        """
         class TestClass:
-            ''' self descriptive'''
+            """
+            Test class
+            """
 
             def a_method(self):
                 return 42
@@ -76,8 +81,8 @@ class TestMemoize(TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with mock.patch.object(TestClass, 'a_method') as mocked:
-            spec = TestClass()
-            spec.a_property
-            spec.a_property
-            mocked.asset_called_once()
+        with mock.patch.object(TestClass, 'a_method') as mockMethod:
+            test = TestClass()
+            self.assertEqual(test.a_property, mockMethod.return_value)
+            self.assertEqual(test.a_property, mockMethod.return_value)
+            mockMethod.assert_called_once()
